f38f79ac8a9f8bd5e3468d70a537acbc7dfa868e
hanan <hananmustafa.hassan@gmail.com>
2021-03-21 06:14:42 -0700

    new changes

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(cors());
 
-app.use((options) => options.AllowAnyOrigin());
+// app.use((options) => options.AllowAnyOrigin());
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
