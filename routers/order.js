899d97def75a08808a34b662c66442c76db76db4
hanan <hananmustafa.hassan@gmail.com>
2021-03-22 11:54:14 -0900

    new changesh

--- routers/order.js
+++ routers/order.js
@@ -80,7 +80,7 @@ routerOrder.delete('/user/:id',authUser ,(req, res) => {
     console.log(id);
     const orderuser=Order.find({_id: id});
     // if(orderuser.order_status!="accepted") {
-        Order.deleteOne({id});
+        Order.deleteOne(id);
         res.send({id});
 
     // }
