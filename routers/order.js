6ba75fa254106f80866297f8de81190ce53cd5a3
hanan <hananmustafa.hassan@gmail.com>
2021-03-22 02:28:28 -0900

    new changesh

--- routers/order.js
+++ routers/order.js
@@ -22,7 +22,7 @@ routerOrder.post('/',UserAuth, async(req, res) => {
 //  Return the Order of specific user 
 routerOrder.get('/user',UserAuth, async(req, res) => {
 
-        const order = await Order.find({userid:req.signedata.id }).populate('items._id');
+        const order = await Order.find({userid:req.signedata.id }).populate('items.menuid');
         const orderwithout = await Order.find({userid:req.signedata.id });
 
         console.log(order[1]?.items);       
