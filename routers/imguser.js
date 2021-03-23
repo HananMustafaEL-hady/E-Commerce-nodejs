0e932bd0690596d9ce81cf8d9f0e0462c6221bfe
hanan <hananmustafa.hassan@gmail.com>
2021-03-23 12:44:00 -0900

    new changesh

--- routers/imguser.js
+++ routers/imguser.js
@@ -92,11 +92,11 @@ routerimg.get('/:id',async(req, res) => {
     console.log(id);
       if(Img.find({menuid:id})){
       
-          if (!upload || upload.length == 0) {
-            return res.status(404).json({
-              err: "No files exist",
-            });
-          }
+         // if (!upload || upload.length == 0) {
+          //  return res.status(404).json({
+           //   err: "No files exist",
+           // });
+        //  }
          // if (upload.contentType === "image/jpeg" || upload.contentType === "img/png") {
             const readStream = gfs.createReadStream(upload.filename);
             readStream.pipe(res);
