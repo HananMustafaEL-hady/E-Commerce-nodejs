73e8a07bb00fae5741f0998dd365d889a65af9b9
hanan <hananmustafa.hassan@gmail.com>
2021-03-23 04:23:37 -0900

    new changesh

--- routers/order.js
+++ routers/order.js
@@ -54,7 +54,7 @@ routerOrder.get('/admin',async(req, res) => {
 routerOrder.patch('/:id',(req, res) => {
     const { id } = req.params;
     console.log(id);
-    const order_status  = req.body;
+    const {order_status } = req.body;
     console.log(req.body);
      Order.findOneAndUpdate({_id:id},{order_status:order_status})
 
